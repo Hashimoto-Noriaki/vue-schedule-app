6173254d4d7f3a63b886244dda0310054d3de7b8
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-18 18:10:54 +0900

    Vue.jsのアプリ初期化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import './styles/index.css'
+import './styles/destyle.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app')
